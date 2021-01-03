b212316c7a48b62f3aaaf98f97c35d6d39dee044
Justyna <justyna.szczepaniec@gmail.com>
2021-01-03 15:11:28 +0100

    Create making-the-menus.py

---
+++ making-the-menus.py
@@ -0,0 +1,28 @@
+# Creating the Menu class that will have the constructor for all our menus.
+
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+  # Giving the Menu class a string representation method:
+  def __repr__(self):
+    return self.name + " menu is available " + str(self.start_time) + " - " + str(self.end_time) + "."
+    
+# Creating 4 different menus:
+brunch_items = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+
+early_bird_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+
+dinner_items = {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}
+
+kids_items = {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}
